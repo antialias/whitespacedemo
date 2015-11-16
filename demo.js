448c332423ac0a45bdf489e239a97e05e33174f9
Thomas Hallock <thomas@1stdibs.com>
2015-11-16 12:11:47 -0500

    which line really changed

--- demo.js
+++ demo.js
@@ -1,5 +1,6 @@
-foo   = require('foo');
-clawz = require('clawz');
-bar   = require('bar');
-fink  = require('fink');
-batty = require('batty');
+foo    = require('foo');
+clawz  = require('clawz');
+bar    = require('bar');
+jewels = require('jewels');
+fink   = require('fink');
+batty  = require('batty');
